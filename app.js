d011243f225e5356341c889a28d144ff972f60ad
Rajesh <rajeshchodisetti.8150@gmail.com>
2023-02-09 22:45:37 +0530

    Before Callback Abstraction

---
+++ app.js
@@ -0,0 +1,32 @@
+const request = require('request')
+const url = 'http://api.weatherstack.com/current?access_key=a4836c1e35f8106fb519f2ffe70a800c&query=37.8267,-122.4233&units=f'
+request({ url: url, json: true }, (error, response) => {
+    if(error){
+        console.log('Unable to connect to weather service')
+    }
+    else if(response.body.error){
+        console.log('Unable to find Location');
+    }
+    else{
+        console.log(response.body.current.weather_descriptions[0]+
+                'It is currently '+response.body.current.temperature +
+                ' degrees out. It feels like '+response.body.current.feelslike+' degrees out')
+    }
+})
+
+const geocodeurl = 'http://api.positionstack.com/v1/forward?access_key=d79e223fa043ca33c23cd2687f08f194&query=1600 Pennsylvania Ave NW, Washington DC'
+request({url: geocodeurl,json: true},(error,response)=>{
+    if(error){
+        console.log('Unable to connect to weather service');
+    }
+    else if(response.body.error){
+        console.log(response.body.error.message);
+    }
+    else{
+        console.log(response.body.data[0].latitude)
+        console.log(response.body.data[0].longitude)
+    }
+    
+})
+
+
